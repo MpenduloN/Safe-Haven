263cb420d7feed4847249406787053a11ef09eb5
MpenduloN <133219249+MpenduloN@users.noreply.github.com>
2025-11-29 08:20:59 +0200

    Create SafeGuardPlatform component as a placeholder

---
+++ src/SafeGuardPlatform.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const SafeGuardPlatfrm = () => {
+  return (
+    <div>
+    <h2>SafeGuardPlatform Compnent</h2>
+      <p>This is a placeholder Component. #Replace</p>
+    </div>
+    );
+};
+
+export default SafeGuardPlatform;
